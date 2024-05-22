c8579a8271c33421dc3b3d106bf49648ed5f264d
DEV270201 <devansh.js@somaiya.edu>
2024-05-21 20:51:07 -0400

    added prisma ORM and generated migration

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,9 @@ async function bootstrap() {
   console.log("Listening on port 3000");
 }
 bootstrap();
+
+
+// Some commands to keep in mind:
+// 1. npx prisma migrate dev -> it will generate tables based on the given schema and push it to the Db
+// 2. npx prisma generate -> it will generate all the types from the given schema
+// 3. npx prisma studio -> it will start the UI for viewing and manipulating records
